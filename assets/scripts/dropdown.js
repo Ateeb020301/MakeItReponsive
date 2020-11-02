9aeddfeb938208151e9ffc1560a660ee3aac8ecc
moha0102 <mohammedak0102@gmail.com>
2020-11-02 10:06:06 +0100

    Fix

---
+++ assets/scripts/dropdown.js
@@ -0,0 +1,42 @@
+const menu = document.getElementById('nav-menu');
+const nav = document.getElementById('nav');
+const bars = document.getElementById('bars');
+
+if (window.outerWidth > 760) {
+  menu.style.display = 'flex';
+  document.documentElement.style.overflow = 'visible';
+  nav.style.backgroundColor = '#F6F7EB';
+  bars.style.color = 'black';
+} else {
+  menu.style.display = "none";
+}
+
+
+window.onresize = function() {
+  if (window.outerWidth > 760) {
+    menu.style.display = 'flex';
+    document.documentElement.style.overflow = 'visible';
+    nav.style.backgroundColor = '#F6F7EB';
+    bars.style.color = 'black';
+  } else {
+    menu.style.display = "none";
+    nav.style.backgroundColor = '#F6F7EB';
+    document.documentElement.style.overflow = 'visible';
+    bars.style.color = 'black';
+  }
+}
+
+
+bars.onclick = function() {
+  if (menu.style.display === 'none') {
+    menu.style.display = "flex";
+    nav.style.backgroundColor = '#212A3B';
+    document.documentElement.style.overflow = 'hidden';
+    bars.style.color = 'white';
+  } else {
+    menu.style.display = "none";
+    document.documentElement.style.overflow = 'visible';
+    nav.style.backgroundColor = '#F6F7EB';
+    bars.style.color = 'black';
+    }
+  }
